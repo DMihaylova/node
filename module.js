ab8812c01998bdbe8b4224a68e702a9d15dd7655
DMihaylova <mihaylova.didi@gmail.com>
2015-05-20 11:48:59 +0300

    Update module.js

--- module.js
+++ module.js
@@ -5,20 +5,38 @@ var circle = function(r) {
 	return Pi*r*r;
 };
 
-//square perimeter
+//circle perimeter
+var circleP = function(r) {
+	return Pi*r*2;
+};
+
+//square area
 var square = function(a) {
-	return 4 * a;
+	return a*2;
+};
+
+//square perimeter
+var squareP = function(a) {
+	return 4*a;
+};
+
+//triangle area
+var triangle = function(b, h) {
+	return 1/2*b*h;
 };
 
 //triangle perimeter
-var triangle = function(a, b, c) {
+var triangleP = function(a, b, c) {
 	return a+b+c;
 };
 
 //it needs to be exported in order to require it from the console
 exports = {
 	circle: circle,
+	circleP: circleP,
 	square: square,
-	triangle: triangle
+	squareP: squareP,
+	triangle: triangle,
+	triangleP: triangleP
 }
 
